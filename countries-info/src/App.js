c69e1df363729dd4e3ba63d03b5b28b91f3f8dd9
NewtonAdhikari <newtonadk@gmail.com>
2022-06-13 21:11:00 +0545

    countries-info step3 completed

--- countries-info/src/App.js
+++ countries-info/src/App.js
@@ -4,6 +4,9 @@ import axios from "axios";
 function App() {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState("");
+  const [city, setCity] = useState("kathmandu");
+  const [weatherInfo, setWeather] = useState({});
+  const key = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
     axios.get("https://restcountries.com/v2/all")
@@ -11,7 +14,16 @@ function App() {
     .catch(err => console.log(err));
   }, [])
 
+  useEffect(() => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then(res => setWeather(res.data))
+    .catch(err => console.log(err));
+  }, [city])
+
   const searchHandler = ({target}) => {
+    const singleCountry = countries.filter(c => c.name.toLowerCase().includes(target.value.toLowerCase()));
+    if(singleCountry.length === 1) setCity(singleCountry[0].capital);
+
     setSearch(target.value);
   }
 
@@ -22,14 +34,19 @@ function App() {
       ? "too many matches found, specify another filter"
       : searchedCountries.map(c => <p key={c.name}>
         {c.name} <button
-          onClick={() => setSearch(c.name)}
+          onClick={() => {
+            setSearch(c.name);
+            setCity(c.capital);
+          }}
         >show</button>
       </p>)
     : ""
 
   if(Array.isArray(displayed) && searchedCountries.length === 1) {
     const {name, capital, area, population, languages, flag} = searchedCountries[0];
-    
+    const {main, wind, weather} = weatherInfo;
+    const icon = weather[0].icon;
+    const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
     displayed = (
       <div>
         <h1>{name}</h1>
@@ -41,6 +58,10 @@ function App() {
           {languages.map(l => <li key={l.name}>{l.name}</li>)}
         </ul>
         <img width="250px" src={flag} alt={name} />
+        <h1>Weather in {city}</h1>
+        <p>temperature: {(main.temp - 273).toFixed(2)} Celcius</p>
+        <p><img src={iconURL} alt={weather[0].main} /></p>
+        <p>wind {wind.speed} m/s</p>
       </div>
     );
   }
